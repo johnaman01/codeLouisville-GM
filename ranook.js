3faf79c3acf88a9bcd59c540bcb15bfad6dca632
John <johnaman01@gmail.com>
2016-10-21 11:15:23 -0400

    cleaned view/; added add & remove to ranook

---
+++ ranook.js
@@ -0,0 +1,31 @@
+//called ranookApp; below is first controller=table;
+angular.module("ranookApp", [])
+  .controller("TableController", function () {
+    var encTable = this;
+    encTable.encounters = [ 
+      {text:"A goblin", done:true}, 
+      {text:"Tiamat", done:true}];
+//add encounters to table array
+    encTable.addEncounter = function () {
+      encTable.encounters.push({text:encTable.encText, done:false});
+      encTable.encText = "";
+    };
+//add index +1 to added values
+    encTable.addNumber = function () {
+      var number = 0;
+      angular.forEach(encTable.encounters, function() {
+        number += encounter.length;
+      });
+      return number;
+    };
+//remove encounters from table array
+    encTable.removeEncounter = function() {
+      var oldEncounters = encTable.encounters;
+      encTable.encounters = [];
+        angular.forEach(oldEncounters, function(encounter) {
+        if (!encounter.done) encTable.encounters.push(encounter);
+      });
+    };
+  });//end controller
+  
+
