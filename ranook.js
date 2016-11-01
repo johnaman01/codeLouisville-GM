66d20b8a66d24a63d2da2718ffdd0f8d08dd91f9
John <johnaman01@gmail.com>
2016-11-01 19:09:49 -0400

    angular not working; switch to mean branch, then delete angular branch

--- ranook.js
+++ ranook.js
@@ -1,31 +1,32 @@
 //called ranookApp; below is first controller=table;
-angular.module("ranookApp", [])
-  .controller("TableController", function () {
-    var encTable = this;
-    encTable.encounters = [ 
-      {text:"A goblin", done:true}, 
-      {text:"Tiamat", done:true}];
+var ranookApp = angular.module("ranookApp", []);
+
+ranookApp.controller("TableController", function 
+TableController($scope) {
+  $scope.encounters = [ 
+    {text:"A goblin", done:true}, 
+    {text:"Tiamat", done:true}];
 //add encounters to table array
-    encTable.addEncounter = function () {
-      encTable.encounters.push({text:encTable.encText, done:false});
-      encTable.encText = "";
-    };
+  $scope.addEncounter = function () {
+    $scope.encounters.push({text:$scope.encText, done:false});
+    $scope.encText = "";
+  };
 //add index +1 to added values
-    encTable.addNumber = function () {
-      var number = 0;
-      angular.forEach(encTable.encounters, function() {
-        number += encounter.length;
-      });
-      return number;
-    };
+  $scope.addNumber = function () {
+    var number = 0;
+    angular.forEach($scope.encounters, function() {
+      number += encounter.length;
+    });
+    return number;
+  };
 //remove encounters from table array
-    encTable.removeEncounter = function() {
-      var oldEncounters = encTable.encounters;
-      encTable.encounters = [];
-        angular.forEach(oldEncounters, function(encounter) {
-        if (!encounter.done) encTable.encounters.push(encounter);
-      });
-    };
-  });//end controller
+  $scope.removeEncounter = function() {
+    var oldEncounters = $scope.encounters;
+    $scope.encounters = [];
+      angular.forEach(oldEncounters, function(encounter) {
+      if (!encounter.done) encTable.encounters.push(encounter);
+    });
+  };
+});//end controller
   
 
